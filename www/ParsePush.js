be67103a2523840439c5cad460061b54335cb68c
nickbarth <nickbarth@live.com>
2014-06-11 22:38:12 -0600

    added global javascript object

--- www/ParsePush.js
+++ www/ParsePush.js
@@ -1,1 +1,12 @@
+var exec = require('cordova/exec');
 
+var ParsePush = {
+  sendNotification: function(data, successCallback, failCallback) {
+    exec(successCallback, failCallback, 'ParsePush', 'sendNotification', [data]);
+  },
+  getNotification: function(data, successCallback, failCallback) {
+    exec(successCallback, failCallback, 'ParsePush', 'getNotification', [data]);
+  }
+}
+
+module.exports = ParsePush;
